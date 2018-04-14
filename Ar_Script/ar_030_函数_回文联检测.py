c55acb9449d2ef8e4f957a36754cfaf1af4f1aea
archerckk <archerckk@163.com>
2018-04-15 07:46:42 +0800

    增加参考答案和其他实现方式

--- Ar_Script/ar_030_函数_回文联检测.py
+++ Ar_Script/ar_030_函数_回文联检测.py
@@ -17,3 +17,18 @@ def huiwenlian(str1):
 
 str1=input('请输入你要检查的内容：')
 huiwenlian(str1)
+
+#其他实现方式
+'''
+.pop()、.append+reverse也是可以实现的
+'''
+
+#参考答案
+# def palindrome(string):
+#     list1 = list(string)
+#     list2 = reversed(list1)
+#     if list1 == list(list2):
+#         return '是回文联!'
+#     else:
+#         return '不是回文联！'
+# print(palindrome('上海自来水来自海上'))
