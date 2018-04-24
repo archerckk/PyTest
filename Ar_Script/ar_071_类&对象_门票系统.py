e9547e2fefc368b9236ef0a87f9807b763ae243b
archerckk <archerckk@163.com>
2018-04-25 07:17:47 +0800

    新增OO思维的门票系统代码

--- Ar_Script/ar_071_类&对象_门票系统.py
+++ Ar_Script/ar_071_类&对象_门票系统.py
@@ -6,20 +6,48 @@ print()
 儿童半票
 '''
 
+# class Ticket:
+#     price=100
+#
+#     def calPrice(self):
+#         time=int(input('请选择你要去的时间（1周末，2为非周末）：'))
+#         adult=int(input('请输入成人人数：'))
+#         children=int(input('请输入小孩人数：'))
+#         if time==1:
+#             self.price=(adult*100+children*100*0.5)*1.2
+#         elif time==2:
+#             self.price=(adult*100+children*100*0.5)
+#         else:
+#             print('你的时间选择有误！！')
+#         return self.price
+#
+# ticket=Ticket()
+# print(ticket.calPrice())
+
+
+'参考答案：(面向对象的思维)'
+
 class Ticket:
-    price=100
 
-    def calPrice(self):
-        time=int(input('请选择你要去的时间（1周末，2为非周末）：'))
-        adult=int(input('请输入成人人数：'))
-        children=int(input('请输入小孩人数：'))
-        if time==1:
-            self.price=(adult*100+children*100*0.5)*1.2
-        elif time==2:
-            self.price=(adult*100+children*100*0.5)
+    def __init__(self,weekend=False,discount=False):
+        self.price=100
+
+        if weekend:
+            self.cost=1.2
         else:
-            print('你的时间选择有误！！')
-        return self.price
+            self.cost=1
 
-ticket=Ticket()
-print(ticket.calPrice())
+        if discount:
+            self.discount=0.5
+        else:
+            self.discount=1
+
+    def calPrice(self,num):
+        total=self.price*self.cost*self.discount*num
+
+        return total
+
+adult=Ticket()
+child=Ticket(discount=True)
+
+print('平时2个成人加1个小孩的票价为%d'%(adult.calPrice(2)+child.calPrice(1)))
