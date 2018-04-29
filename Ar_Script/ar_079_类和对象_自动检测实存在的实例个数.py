0b080b7785191b8914a20c5d90df606fef920fbf
archerckk <archerckk@163.com>
2018-04-29 09:20:35 +0800

    完成40讲课后练习

--- Ar_Script/ar_079_类和对象_自动检测实存在的实例个数.py
+++ Ar_Script/ar_079_类和对象_自动检测实存在的实例个数.py
@@ -1,19 +1,19 @@
 class Demo:
-
+    count=0
     def __init__(self):
-        self.judge=True
-        self.count=0
-        # if self.__init__():
-        #     self.count+=1
+        Demo.count+=1
+
 
 
     def __del__(self):
 
-        self.count-=1
+        Demo.count-=1
 
 
 
 d=Demo()
 c=Demo()
 
-print(c.count)
+print(Demo.count)
+del c
+print(Demo.count)
--- Ar_Script/ar_080_类和对象_模拟栈对象.py
+++ Ar_Script/ar_080_类和对象_模拟栈对象.py
@@ -1,8 +1,14 @@
+'''
+栈的话是后进先出
+'''
+
 class Stack:
 
-    def __init__(self):
+    def __init__(self,start=[]):
         self.x=[]
-
+        for i in start:
+            self.push(i)
+        '很巧妙的将传进来的参数放入到x列表里面了'
 
     def isEmpty(self):
         if len(self.x):
@@ -10,16 +16,30 @@ class Stack:
         else:
             return True
 
+    '后进先出应该是用append'
     def push(self,factor):
-        self.x.insert(0,factor)
+       self.x.append(factor)
+        # self.x.insert(0,factor)
 
+    ' 后进先出，所以还是删最后一个，self.x.remove(self.x[0])'
     def pop(self,factor):
-        self.x.remove(self.x[0])
+        self.x.pop()
 
+        '对于删除，查询要主要对为空的情况进行判断和处理'
+        # if not self.stack:
+        #     print('警告：栈为空！')
+        # else:
+        #     return self.stack.pop()
+
+
+    '可以用-1来表示列表的最后一个元素，不用len（obj）-1'
     def top(self):
-        return self.x[0]
+        return self.x[-1]
 
     def bottom(self):
-        return self.x[len(self.x)-1]
+        return self.x[0]
 
-stack=Stack()
+stack=Stack(start=[123,456])
+print(stack.isEmpty())
+
+
