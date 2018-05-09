4359b3b9414aa0c7211603c324484bbf6534b5b7
archerckk <archerckk@163.com>
2018-05-09 11:41:25 +0800

    抓取地址换回贴吧，更正正则表达式

--- Ar_Script/ar_120_爬虫_贴图单网页图片抓取.py
+++ Ar_Script/ar_120_爬虫_贴图单网页图片抓取.py
@@ -1,8 +1,6 @@
 import urllib.request
 import re
 import random
-import requests
-from bs4 import BeautifulSoup
 
 def get_Proxy(filename):
     Proxy_list=[]
@@ -14,38 +12,33 @@ def get_Proxy(filename):
 
 def get_url(url):
     req=urllib.request.Request(url)
-    # req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36')
-    # proxys=get_Proxy('result/ip_proxy.txt')
-    # proxy=random.choice(proxys)
-    #
-    # proxy_support =urllib.request.ProxyHandler({'http':proxy})
-    # opener=urllib.request.build_opener(proxy_support)
-    # response=opener.open(req)
-    # html=response.read().decode('utf-8')
+    req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36')
+    proxys=get_Proxy('result/ip_proxy.txt')
+    proxy=random.choice(proxys)
 
-    res=requests.get('http://jandan.net/ooxx/page-62#comments')
-    html=BeautifulSoup(res.text)
-    # print(html)
-
-
+    proxy_support =urllib.request.ProxyHandler({'http':proxy})
+    opener=urllib.request.build_opener(proxy_support)
+    response=opener.open(req)
+    html=response.read().decode('utf-8')
 
     return html
 
 def get_img(url):
     html=get_url(url)
-    print(html)
-    # img_list=re.findall(re.compile(r'<img src="http://([^"]+\.jpg)"'), html)
+    # print(html)
+    img_list=re.findall(re.compile(r'<img class="BDE_Image" src="(https://[^"]+\.jpg)"'), html)
 
-    # for i in img_list:
-    #     '取到最后一个反斜杠作为分隔符，保存文件名'
-    #     print(i)
-        # filename=i.split('/')[-1]
-        # urllib.request.urlretrieve(i,'result/%s'%filename,None)
+    for i in img_list:
+        '取到最后一个反斜杠作为分隔符，保存文件名'
+        # print(i)
+        filename=i.split('/')[-1]
+        urllib.request.urlretrieve(i,'result/%s'%filename,None)
 
+    '测试代码:'
     # for i in img_list:
     #     print(i)
 
 
 if __name__ == '__main__':
-    url = 'http://jandan.net/ooxx/page-62#comments'
+    url = 'https://tieba.baidu.com/p/3426953355'
     get_img(url)
