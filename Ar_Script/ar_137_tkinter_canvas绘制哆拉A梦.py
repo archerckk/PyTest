30478e992cafe1d8d8368159b652dbb4935942a6
archerckk <archerckk@163.com>
2018-05-17 19:58:27 +0800

    新增画布绘画代码，部分绘制哆啦A梦代码

--- Ar_Script/ar_137_tkinter_canvas绘制哆拉A梦.py
+++ Ar_Script/ar_137_tkinter_canvas绘制哆拉A梦.py
@@ -7,17 +7,30 @@ c.pack()
 c.create_line(0,300,600,300,fill='gray',dash=(4,4))
 c.create_line(300,0,300,600,fill='gray',dash=(4,4))
 
-
+'头和脸'
 c.create_oval(150,80,450,380,fill='#009fe8',outline='black')
 c.create_oval(180,140,420,380,fill='white',outline='black')
 
+'眼眶'
 c.create_oval(250,120,300,180,fill='white',outline='black')
 c.create_oval(300,120,350,180,fill='white',outline='black')
 
+'眼球'
 c.create_oval(280,140,295,160,fill='black',outline='black')
 c.create_oval(305,140,320,160,fill='black',outline='black')
 
+'眼珠'
+c.create_oval(285,145,292,155,fill='white',outline='black')
+c.create_oval(307,145,314,155,fill='white',outline='black')
 
+'鼻子'
+c.create_oval(290,165,310,185,fill='red',outline='black')
+
+'竖线'
+c.create_line(300,185,300,295,fill='black')
+
+'嘴巴'
+c.create_arc(180,295,600,295,fill='black',style='arc')
 
 
 mainloop()
