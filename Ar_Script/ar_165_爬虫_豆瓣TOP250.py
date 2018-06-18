4428860345b95c71f5307acee78673a043df07a4
archerckk <archerckk@163.com>
2018-06-18 09:19:58 +0800

    豆瓣代码完整版

--- Ar_Script/ar_165_爬虫_豆瓣TOP250.py
+++ Ar_Script/ar_165_爬虫_豆瓣TOP250.py
@@ -8,21 +8,72 @@ def url_open(url):
 
     return res
 
+
+def find_movies(res):
+    soup = bs(res.text, 'html.parser')
+
+    '电影名'
+    movies=[]
+    targets=soup.find_all('div',class_='hd')
+    for i in targets:
+        movies.append(i.a.span.text+'  ')
+
+
+    '评分'
+    score=[]
+    targets = soup.find_all('span', class_='rating_num')
+    for i in targets:
+        score.append('评分：%s  '%i.text)
+
+
+    '电影介绍'
+    info=[]
+    targets=soup.find_all('div',class_='bd')
+    for i in targets:
+        try:
+            info.append(i.p.text.split('\n')[1].strip()+i.p.text.split('\n')[2].strip())
+        except:
+            continue
+
+
+    '显示结果'
+    result=[]
+    length=len(movies)
+    for  i in range(length):
+        result.append(movies[i]+score[i]+info[i]+'\n')
+
+
+    return result
+
+
+
+
 def find_depth(res):
     soup = bs(res.text, 'html.parser')
-    depth=soup.find('span',class_='next')
+    depth=soup.find('span',class_='next').previous_sibling.previous_sibling.text
 
-    return depth
+    return int(depth)
 
 
 def main():
     host='https://movie.douban.com/top250'
     res=url_open(host)
-    # soup = bs(res.text, 'html.parser')
+
+    '获取一个要爬取的页面总数'
     depth=find_depth(res)
-    print(depth)
-    # print(soup.prettify())
+
+    result=[]
+    for i in range(depth):
+        url=host+'?start=%d&filter='%(25*i)
+        res=url_open(url)
+        result.extend(find_movies(res))
+
+
+    with open('result/douban.txt','w',encoding='utf-8')as f:
+        f.writelines(result)
 
 
 
-main()
+
+if __name__ == '__main__':
+    main()
