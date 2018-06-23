604040537f5a6cff9a0d81cccab0061db320801d
archerckk <archerckk@163.com>
2018-06-23 09:47:45 +0800

    新增对齐，列宽，字体设置

--- Ar_Script/ar_165_爬虫_豆瓣TOP250.py
+++ Ar_Script/ar_165_爬虫_豆瓣TOP250.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import openpyxl
+from openpyxl.styles import NamedStyle,Alignment,Font,PatternFill
+from openpyxl.styles.colors import RED
+
 
 def url_open(url):
     header= {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36'}
@@ -64,11 +67,52 @@ def find_depth(res):
 
 def save_excel(result):
     wb=openpyxl.Workbook()
+    wb.guess_type=True
     ws=wb.active
+
+
+    '标题样式'
+    title_style=NamedStyle(name='title_style')
+    title_style.alignment=Alignment(horizontal='center',vertical='center')
+    title_style.font=Font(size=14,bold=True)
+    title_style.fill=PatternFill(fill_type='solid',fgColor='b2b2b2')
+
+
+    '内容样式'
+    content_style = NamedStyle(name='content_style')
+    content_style.alignment = Alignment(horizontal='center', vertical='center')
+    content_style.font = Font(size=12)
+
+    '注册标题和内容样式'
+    wb.add_named_style(title_style)
+    wb.add_named_style(content_style)
+
     ws.append(['电影名','评分','电影介绍'])
+    for i in ws.rows:
+
+        ws[i[0].coordinate].style=title_style
+        ws.column_dimensions['A'].width=20
+        ws[i[1].coordinate].style = title_style
+        ws.column_dimensions['B'].width = 20
+        ws[i[2].coordinate].style = title_style
+        ws.column_dimensions['C'].width = 150
+
 
     for i in result:
         ws.append(i)
+
+
+    for i in ws.iter_rows(min_row=2,min_col=1,max_row=251,max_col=3):
+        ws[i[0].coordinate].style=content_style
+        ws.column_dimensions['A'].width = 20
+        # ws[i[1].coordinate].number_format = '[>9.5][RED]#.0分'
+        ws[i[1].coordinate].style = content_style
+        ws.column_dimensions['B'].width = 20
+        ws[i[2].coordinate].style = content_style
+        ws.column_dimensions['C'].width = 150
+
+
+
 
     wb.save('result/豆瓣top250.xlsx')
 
