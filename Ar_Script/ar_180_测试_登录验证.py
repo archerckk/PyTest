ceb8ed4da4fceb60576d757b237d7fd9d2347fd9
archerckk <archerckk@163.com>
2018-07-03 17:05:31 +0800

    整理格式，优化显示效果

--- Ar_Script/ar_180_测试_登录验证.py
+++ Ar_Script/ar_180_测试_登录验证.py
@@ -1,17 +1,17 @@
 from selenium import webdriver
 import time
 
-driver=webdriver.Chrome()
+driver = webdriver.Chrome()
 driver.get('https://mail.qq.com/')
 
-account='137160564'
-psw='chaoheweijing'
-title=driver.title
-url=driver.current_url
+account = '137160564'
+psw = 'chaoheweijing'
+title = driver.title
+url = driver.current_url
 
-print('-'*100)
-print('登录前标题：%s'%title)
-print('登录前网址：%s'%url)
+print('=' * 150)
+print('登录前标题：%s' % title)
+print('登录前网址：%s' % url)
 
 driver.switch_to.frame('login_frame')
 driver.find_element_by_id('u').clear()
@@ -21,14 +21,13 @@ driver.find_element_by_id('p').send_keys(psw)
 driver.find_element_by_id('login_button').click()
 time.sleep(5)
 
-title=driver.title
-url=driver.current_url
+title = driver.title
+url = driver.current_url
 
-
-print('-'*100)
-print('登录后标题：%s'%title)
-print('登录后网址：%s'%url)
-user=driver.find_element_by_id('useraddr').text
-print('登录的账号为：%s'%user)
+print('=' * 150)
+print('登录后标题：%s' % title)
+print('登录后网址：%s' % url)
+user = driver.find_element_by_id('useraddr').text
+print('登录的账号为：%s' % user)
 
 driver.quit()
