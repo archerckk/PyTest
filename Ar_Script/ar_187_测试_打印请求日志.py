02478198330bed62df4f64d53f77b57eb506c130
archerckk <archerckk@163.com>
2018-07-10 08:57:07 +0800

    新增打印日志代码

---
+++ Ar_Script/ar_187_测试_打印请求日志.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+driver=webdriver.Chrome()
+
+driver.get('http://www.baidu.com')
+
+driver.find_element_by_id('kw').send_keys('Selenium')
+driver.find_element_by_id('su').click()
+
+driver.quit()
