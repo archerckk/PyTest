6583a7d8cf7b1f6d5d679390ecd3459aae358044
archerckk <archerckk@163.com>
2018-10-18 16:27:45 +0800

    筛选无用多语言

--- Ar_Script/ar_214_测试_多语言xml修改.py
+++ Ar_Script/ar_214_测试_多语言xml修改.py
@@ -2,7 +2,9 @@ from xml.etree import ElementTree as et
 
 '将没有用的xml里面的所有string标签的name属性都拿出来'
 
-
+# target_file=et.parse('resources/香港多语言新加的.xml')
+# target_root=et.XML(str_xml)
+# target_root=target_file.getroot()
 xml_file=et.parse('resources/gosms.xml')
 root=xml_file.getroot()
 count=0
@@ -13,14 +15,25 @@ for i in root:
     name_dict=i.attrib
     attr_list.append(name_dict['name'])
 
+str_xml = open('resources/台湾多语言新加的.txt', 'r', encoding='GBK')
 
-for i in root.findall('string'):
-    if i.attrib['name'] in attr_list:
-        # root.remove(i)
-        print(i.attrib['name'])#测试代码
-# for i in root2:
-#     print(i.attrib)
-# xml_file.write('resources/gosms.xml')
+with open('resources/new_target2.txt','w',encoding='GBK')as f:
+
+    for i in str_xml:
+        a=i.partition('name="')
+        b=a[2].partition('"')
+        if b[0] not in attr_list:
+            f.write(i)
+
+
+
+# for i in target_file.findall('string'):
+#     if i.attrib['name'] in attr_list:
+#         target_root.remove(i)
+#         # print(i.attrib['name'])#测试代码
+# # for i in root2:
+# #     print(i.attrib)
+# target_file.write('resources/香港多语言新加的.xml')
 
 '打印所有属性值'
 # for i in attr_list:
