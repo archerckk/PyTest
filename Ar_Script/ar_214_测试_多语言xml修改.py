7c6a531cfd8b4afdd812f7a7e73efa7364443942
archerckk <archerckk@163.com>
2018-10-18 16:32:41 +0800

    优化代码结构

--- Ar_Script/ar_214_测试_多语言xml修改.py
+++ Ar_Script/ar_214_测试_多语言xml修改.py
@@ -5,27 +5,25 @@ from xml.etree import ElementTree as et
 # target_file=et.parse('resources/香港多语言新加的.xml')
 # target_root=et.XML(str_xml)
 # target_root=target_file.getroot()
-xml_file=et.parse('resources/gosms.xml')
-root=xml_file.getroot()
-count=0
-attr_list=[]
+xml_file = et.parse('resources/gosms.xml')
+root = xml_file.getroot()
+attr_list = []
+# count=0
+
 
 '获取所有的string的name属性值放入列表'
 for i in root:
-    name_dict=i.attrib
+    name_dict = i.attrib
     attr_list.append(name_dict['name'])
 
 str_xml = open('resources/台湾多语言新加的.txt', 'r', encoding='GBK')
 
-with open('resources/new_target2.txt','w',encoding='GBK')as f:
-
+with open('resources/new_target2.txt', 'w', encoding='GBK')as f:
     for i in str_xml:
-        a=i.partition('name="')
-        b=a[2].partition('"')
+        a = i.partition('name="')
+        b = a[2].partition('"')
         if b[0] not in attr_list:
             f.write(i)
-
-
 
 # for i in target_file.findall('string'):
 #     if i.attrib['name'] in attr_list:
