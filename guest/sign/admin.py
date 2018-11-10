8f15a54f702c8c74fe77be531344e2b290854781
archerckk <archerckk@163.com>
2018-11-10 09:49:51 +0800

    新增对齐，列宽，字体设置

--- guest/sign/admin.py
+++ guest/sign/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from sign.models import Event,Guest
 # Register your models here.
+
+
+
+# admin.site.register(Event)
+# admin.site.register(Guest)
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id','name','status','address','start_time']
+    search_fields = ['name']#搜索栏
+    list_filter = ['status']#过滤器
+class GuessAdmin(admin.ModelAdmin):
+    list_display = ['realname','email','sign','create_time','event']
+    search_fields = ['phone','realname']#搜索栏
+    list_filter = ['sign'] ##过滤器
+
+
+admin.site.register(Event,EventAdmin)
+admin.site.register(Guest,GuessAdmin)
