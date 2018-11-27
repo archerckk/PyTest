7ee607aeba899ffeb259983e6cf07b0bdcafa770
archerckk <archerckk@163.com>
2018-11-27 23:40:20 +0800

    恢复测试代码

--- guest/guest/settings.py
+++ guest/guest/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',# 解决Django CSRF mechanism错误，直接忽略这个风险
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -4,38 +4,38 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello Django!!!')
-
 # def index(request):
-#     return render(request,'index.html')
+#     return HttpResponse('Hello Django!!!')
+
+def index(request):
+    return render(request,'index.html')
 
 #登陆动作
-# def login_action(request):
-#     if request.method=='POST':
-#         '通过request.POST获取POST请求，通过.get()方法获取username属性，如果为空则返回空字符串'
-#         username=request.POST.get('username','')
+def login_action(request):
+    if request.method=='POST':
+        '通过request.POST获取POST请求，通过.get()方法获取username属性，如果为空则返回空字符串'
+        username=request.POST.get('username','')
+
+        password=request.POST.get('password','')
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            response = HttpResponseRedirect('/event_manage')
+            response.set_cookie('user',username,3600)#添加浏览器cookie
+            return response
+        # if username=='123' and password=='123':
+        #     # return HttpResponse('登陆成功！')
+        #     # return HttpResponseRedirect('/event_manage/')
+        #     response=HttpResponseRedirect('/event_manage')
+        #     # response.set_cookie('user',username,3600)#添加浏览器cookie
+        #     request.session['user']=username#将session信息记录到浏览器
+        #     return response
+        else:
+            return render(request,'index.html',{'error':'用户名或者密码错误！'})
 #
-#         password=request.POST.get('password','')
-#         user=auth.authenticate(username=username,password=password)
-#         if user is not None:
-#             auth.login(request,user)
-#             response = HttpResponseRedirect('/event_manage')
-#             response.set_cookie('user',username,3600)#添加浏览器cookie
-#             return response
-#         # if username=='123' and password=='123':
-#         #     # return HttpResponse('登陆成功！')
-#         #     # return HttpResponseRedirect('/event_manage/')
-#         #     response=HttpResponseRedirect('/event_manage')
-#         #     # response.set_cookie('user',username,3600)#添加浏览器cookie
-#         #     request.session['user']=username#将session信息记录到浏览器
-#         #     return response
-#         else:
-#             return render(request,'index.html',{'error':'用户名或者密码错误！'})
-#
-# #发布会管理
-# @login_required
-# def event_manage(request):
-#     username=request.COOKIES.get('user','')#读取浏览器cookie
-#     # username=request.session.get('user','')#读取浏览器session
-#     return render(request,'event_manage.html',{'user':username})
+#发布会管理
+@login_required
+def event_manage(request):
+    username=request.COOKIES.get('user','')#读取浏览器cookie
+    # username=request.session.get('user','')#读取浏览器session
+    return render(request,'event_manage.html',{'user':username})
