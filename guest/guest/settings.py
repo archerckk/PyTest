6e78bfd8faa734ad522cc6992747bf35396dc38d
archerckk <archerckk@163.com>
2018-12-11 09:13:16 +0800

    提交第四章部分代码

--- guest/guest/settings.py
+++ guest/guest/settings.py
@@ -74,10 +74,24 @@ WSGI_APPLICATION = 'guest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'NAME':'guest',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'OPTIONS':{
+            'init_command':'SET sql_mode="STRICT_TRANS_TABLES"'#对于InnoDB表，sql插入执行失败，会报错，全部回滚。
+        }
     }
 }
 
