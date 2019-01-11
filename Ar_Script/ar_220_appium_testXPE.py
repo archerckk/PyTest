4187df65bc871ccf6a48b74b90a92763034c0428
archerckk <archerckk@163.com>
2019-01-12 07:17:41 +0800

    完善测试代码

--- Ar_Script/ar_220_appium_testXPE.py
+++ Ar_Script/ar_220_appium_testXPE.py
@@ -11,9 +11,9 @@ class SearchTest(unittest.TestCase):
     def setUp(self):
         desired_caps = {}
         desired_caps['automationName'] = 'Appium'
-        desired_caps['deviceName'] = '98891936513437444f'
+        desired_caps['deviceName'] = '68cac4b1'
         desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '8.0'
+        desired_caps['platformVersion'] = '4.4.4'
         desired_caps['noReset'] = True
         desired_caps["appPackage"] = "com.picstudio.photoeditorplus"
         desired_caps["appActivity"] = "com.picstudio.photoeditorplus.camera.MainActivity"
@@ -22,9 +22,9 @@ class SearchTest(unittest.TestCase):
         sleep(5)
 
         '按4下返回键退出订阅页'
-        # for i in range(2):
-        #     self.driver.press_keycode(4)
-        #     sleep(1)
+        for i in range(2):
+            self.driver.press_keycode(4)
+            sleep(0.5)
 
     def test_case(self):
         driver=self.driver
@@ -37,77 +37,81 @@ class SearchTest(unittest.TestCase):
         #     driver.press_keycode(4)
         #     sleep(1)
         '点击主页collage icon'
-        collage=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/tc'][@text='COLLAGE']")
-        collage.click()
+        icon_str="//*[@resource-id='com.picstudio.photoeditorplus:id/ql'][text='X Photo Editor']"
+        icon=driver.find_element_by_xpath(icon_str)
+        print(icon.rect)
 
-        '处理进入相册的全屏广告'
-        if 'android.webkit.WebView' in driver.page_source:
-            driver.press_keycode(4)
-            sleep(2)
-            driver.press_keycode(4)
-
-        '在相册选择同一张照片3张'
-
-        pic_out=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/a4h'][@text='Start']/../..")
-        pic_frame=pic_out.find_element_by_class_name('android.widget.FrameLayout')
-        pic_tmp=pic_frame.find_element_by_class_name('android.widget.RelativeLayout')
-        pic_list=pic_tmp.find_element_by_class_name('android.widget.ListView')
-        pic_list_position=pic_list.rect
-
-        origin_album=pic_list.find_element_by_xpath("//*[@class='android.widget.LinearLayout'][@index=0]")
-        origin_album_position=origin_album.rect
-        print(origin_album_position)
-
-        origin_pic=pic_list.find_element_by_xpath("//*[@class='android.widget.LinearLayout'][@index=1]")
-        origin_pic_position=origin_pic.rect
-        print(origin_pic_position)
-        origin_pic_x=(origin_pic_position['width']-18)/3/2
-
-        origin_pic_y=(origin_pic_position['y']+(origin_pic_position['height']/2))
-
-        target_x=origin_pic_x+((origin_pic_position['width']-18)/3)*1
-        target_y=origin_pic_y+origin_pic_position['height']*0
-
-
-        print(target_x,target_y)
-        for i in range(3):
-            driver.tap([(target_x,target_y)])
-            sleep(0.5)
-
-        '点击开始拼图'
-        start=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/a4h'][@text='Start']")
-        start.click()
-        sleep(3)
-
-        '切换到拼图tab'
-        change=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/f3'][@index=2][@class='android.widget.ImageView']")
-        change.click()
-        sleep(0.5)
-        '保存图片'
-        # out_side_change=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/f3'][@index=2]/..")
+        # collage=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/tc'][@text='COLLAGE']")
+        # collage.click()
         #
-        # confirm=out_side_change.find_element_by_xpath("//*[@class='android.widget.ImageView'][4]")
-
-        confirm=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/gc']")
-        confirm.click()
-        sleep(5)
-        '按返回键关闭广告'
-        if 'android.webkit.WebView' in driver.page_source:
-            driver.press_keycode(4)
-            sleep(2)
-
-        if 'Not Really'in driver.page_source:
-            driver.press_keycode(4)
-            sleep(2)
-
-        if 'Save to album' not in driver.page_source:
-            assert print('拼图保存失败')
-        else:
-            print('测试通过，正确保存拼图')
-
-        '保存截图'
-        driver.save_screenshot(screen_shot)
-        sleep(3)
+        # '处理进入相册的全屏广告'
+        # if 'android.webkit.WebView' in driver.page_source:
+        #     driver.press_keycode(4)
+        #     sleep(2)
+        #     driver.press_keycode(4)
+        #
+        # '在相册选择同一张照片3张'
+        #
+        # pic_out=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/a4h'][@text='Start']/../..")
+        # pic_frame=pic_out.find_element_by_class_name('android.widget.FrameLayout')
+        # pic_tmp=pic_frame.find_element_by_class_name('android.widget.RelativeLayout')
+        # pic_list=pic_tmp.find_element_by_class_name('android.widget.ListView')
+        # pic_list_position=pic_list.rect
+        #
+        # origin_album=pic_list.find_element_by_xpath("//*[@class='android.widget.LinearLayout'][@index=0]")
+        # origin_album_position=origin_album.rect
+        # print(origin_album_position)
+        #
+        # origin_pic=pic_list.find_element_by_xpath("//*[@class='android.widget.LinearLayout'][@index=1]")
+        # origin_pic_position=origin_pic.rect
+        # print(origin_pic_position)
+        # origin_pic_x=(origin_pic_position['width']-18)/3/2
+        #
+        # origin_pic_y=(origin_pic_position['y']+(origin_pic_position['height']/2))
+        #
+        # target_x=origin_pic_x+((origin_pic_position['width']-18)/3)*1
+        # target_y=origin_pic_y+origin_pic_position['height']*0
+        #
+        #
+        # print(target_x,target_y)
+        # for i in range(3):
+        #     driver.tap([(target_x,target_y)])
+        #     sleep(0.5)
+        #
+        # '点击开始拼图'
+        # start=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/a4h'][@text='Start']")
+        # start.click()
+        # sleep(3)
+        #
+        # '切换到拼图tab'
+        # change=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/f3'][@index=2][@class='android.widget.ImageView']")
+        # change.click()
+        # sleep(0.5)
+        # '保存图片'
+        # # out_side_change=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/f3'][@index=2]/..")
+        # #
+        # # confirm=out_side_change.find_element_by_xpath("//*[@class='android.widget.ImageView'][4]")
+        #
+        # confirm=driver.find_element_by_xpath("//*[@resource-id='com.picstudio.photoeditorplus:id/gc']")
+        # confirm.click()
+        # sleep(5)
+        # '按返回键关闭广告'
+        # if 'android.webkit.WebView' in driver.page_source:
+        #     driver.press_keycode(4)
+        #     sleep(2)
+        #
+        # if 'Not Really'in driver.page_source:
+        #     driver.press_keycode(4)
+        #     sleep(2)
+        #
+        # if 'Save to album' not in driver.page_source:
+        #     assert print('拼图保存失败')
+        # else:
+        #     print('测试通过，正确保存拼图')
+        #
+        # '保存截图'
+        # driver.save_screenshot(screen_shot)
+        # sleep(3)
 
     # def test_case2(self):
     #     driver=self.driver
