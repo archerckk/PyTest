c8f0175f66275ead7abedba630a04cd1cd19008a
archerckk <archerckk@163.com>
2019-03-07 08:27:20 +0800

    新增IP列表配置

--- Ar_Script/ar_238_pymysql_循环插入mysql数据.py
+++ Ar_Script/ar_238_pymysql_循环插入mysql数据.py
@@ -1,9 +1,10 @@
 from pymysql import connect,cursors
 from random import randint
 
+host_list=['192.168.85.136','127.0.0.1','61.140.112.160']
 
-conn=connect(host='127.0.0.1',
-             port=3307,
+conn=connect(host=host_list[2],
+             port=3306,
              user='root',
              password='123456',
              db='test_data',
@@ -11,15 +12,15 @@ conn=connect(host='127.0.0.1',
              cursorclass=cursors.DictCursor)
 
 try:
-    with conn.cursor()as cursor:
-        for i in range (1,1000):
-            id="%03d"%i
-            sex=randint(0,1)
-            age=randint(12,70)
-            sql='INSERT INTO test_user(id,name,sex,age)VALUES (%d,"user_%s",%d,%d)'\
-                %(i,id,sex,age)
-            cursor.execute(sql)
-    conn.commit()
+    # with conn.cursor()as cursor:
+    #     for i in range (1,1000):
+    #         id="%03d"%i
+    #         sex=randint(0,1)
+    #         age=randint(12,70)
+    #         sql='INSERT INTO test_user(id,name,sex,age)VALUES (%d,"user_%s",%d,%d)'\
+    #             %(i,id,sex,age)
+    #         cursor.execute(sql)
+    # conn.commit()
 
     with conn.cursor()as cursor:
         sql='SELECT * from test_user WHERE name=%s'
