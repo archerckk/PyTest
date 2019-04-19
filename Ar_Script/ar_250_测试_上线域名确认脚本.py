b1572e06a4d5cd9c116289c63636346f1bd0f86e
archerckk@163.com <a3203589>
2019-04-19 18:43:35 +0800

    monkey列表选择代码启用

--- Ar_Script/ar_250_测试_上线域名确认脚本.py
+++ Ar_Script/ar_250_测试_上线域名确认脚本.py
@@ -6,17 +6,17 @@ import signal
 import  subprocess
 
 def get_info():
-    command='adb shell logcat |findstr nad >log.txt'
-    os.system(command)
-    time.sleep(3)
-    signal.signal(signal.SIGTERM, exit)
+        command='adb shell logcat |findstr nad >log.txt'
+        os.system(command)
+        time.sleep(3)
+
 
 
 # result=subprocess.Popen(command,shell=True,stdout=subprocess.PIPE).stdout
 # print(result.read())
 
 result=get_info()
-
+# signal.signal(signal.SIGTERM, get_info())
 print('测试成功')
 # time.sleep(15)
 
