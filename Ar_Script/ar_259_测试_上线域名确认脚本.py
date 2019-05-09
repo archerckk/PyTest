19f7490a604c6e4168075c15abb17db7c14fc8a1
archerckk@163.com <a3203589>
2019-05-09 21:24:47 +0800

    完善卸载，跟安装app还有启动app逻辑

--- Ar_Script/ar_259_测试_上线域名确认脚本.py
+++ Ar_Script/ar_259_测试_上线域名确认脚本.py
@@ -3,12 +3,13 @@ import subprocess
 import time
 import easygui as g
 import re
-
+import threading
 """
 1.获取文件的路径
 2.用路径获取到包名
 3.卸载包名
 4.安装app
+    新建日志开启文件
 5.先开启日志抓取
 5.打开app
 6.分析日志文件抓取各个域名
@@ -17,11 +18,41 @@ import re
     通过包名
 """
 
+def openLog():
+    add_debug_list = [
+        "adb shell touch sdcard/abcxxxtestmodefilexxx",
+        "adb shell touch sdcard/logger.ChargerSdk.debug",
+        "adb shell touch sdcard/logger.CleanerSdk.debug",
+        "adb shell touch sdcard/logger.DefenderSdk.debug",
+        "adb shell touch sdcard/logger.CommonSdk.debug",
+        "adb shell touch sdcard/logger.CoverSdk.debug",
+        "adb shell touch sdcard/logger.innerSdk.debug",
+        "adb shell touch sdcard/logger.AnalyticsSdk.debug",
+        "adb shell touch /sdcard/stoooooorm",
+        "adb shell touch sdcard/moooooon",
+        "adb shell touch sdcard/appsurfacetestmode",
+        "adb shell touch sdcard/xxtestmodexx"
+    ]
+    for i in add_debug_list:
+        os.popen(i)
+    print("files creat successful!")
+
 def getPackagInfo():
     msg='请选择你要检查的apk安装包'
     title='文件选择'
     default="*.apk"
+
     filePath=g.fileopenbox(msg=msg,title=title,default=default)
+    if ' ' in filePath:
+        os.rename(filePath, filePath.replace(' ', '_'))
+        filePath = filePath.replace(' ', '_')
+    if "&" in filePath:
+        os.rename(filePath, filePath.replace('&', '_'))
+        filePath = filePath.replace('&', '_')
+    if "&&" in filePath:
+        os.rename(filePath, filePath.replace('&&', '_'))
+        filePath = filePath.replace('&&', '_')
+
     print(filePath)
     command='aapt dumpsys badging %s > packageInfo.txt'%filePath
     packInfoFile='./packageInfo.txt'
@@ -50,24 +81,78 @@ def uninstallApp(packageName):
 
     return handle
 
+def installapp(packagePath):
+    command='adb install %s'%packagePath
+    handle=subprocess.Popen(command,shell=True,stdout=subprocess.PIPE)
+
+    return handle
+
+def starApp(packageName,lanuchableActivity):
+    handle = subprocess.Popen(r'adb shell am start %s/%s' % (packageName,lanuchableActivity))
+
+    return handle
+
 def judgeRunning(function):
     while True:
-
-
         if function.poll()!=None:
-            print('指令执行完成')
+            print('进程已终止')
             break
         else:
             continue
 
-# filePath,packageName,lanuchableActivity=getPackagInfo()
-# handle=uninstallApp(packageName)
-handle=subprocess.Popen("ping www.baidu.com",shell=True,stdout=subprocess.PIPE)
-a=handle.stdout.read()
-a=a.decode('gbk')
-print(a)
+def get_log():
+    handle = subprocess.Popen("adb shell  logcat  >log.txt " , shell=True)
+    print('\n正在执行log截取，请等待15秒左右')
+    time.sleep(15)
+    result=subprocess.Popen("taskkill /F /T /PID %s"% str(handle.pid) , stdout=subprocess.PIPE,shell=True)
+    # print('日志获取1执行完成')
+
+
+
+# #测试代码
+# handle=subprocess.Popen("ping www.baidu.com",shell=True,stdout=subprocess.PIPE)
+# a=handle.stdout.read()
+# a=a.decode()
+# print(a)
+
+filePath,packageName,lanuchableActivity=getPackagInfo()
+handle=uninstallApp(packageName)
+uninstallApp(handle)
+judgeRunning(handle)
+print('%s 卸载成功'%packageName)
+print('%s 开始安装，请稍后'%packageName)
+handle_install=installapp(filePath)
+judgeRunning(handle_install)
+print('%s 安装成功'%packageName)
+
+#配置线程
+threads=[]
+# t1=threading.Thread(target=get_log1,args=(product,))
+t1=threading.Thread(target=get_log)
+t2=threading.Thread(target=starApp,args=[packageName,lanuchableActivity])
+
+threads.append(t1)
+threads.append(t2)
+
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()
+
+i = 2
+for t in threads:
+    while 1:
+        if t.is_alive():
+            continue
+        else:
+            i-=1
+            print('线程运行数为：',i)
+            break
+
+print('线程关闭完毕')
+
+
+
 # print(a.decode('utf-8'))
 # print(str(handle.stdout.encode("utf-8")))
-
-
-# judgeRunning(handle)
