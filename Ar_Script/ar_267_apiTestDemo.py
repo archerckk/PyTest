35b2678d93e70b1b9bdedf3d061b7fe924cffa8d
archerckk <archerckk@163.com>
2019-05-18 15:06:35 +0800

    邮件发送完善

--- Ar_Script/ar_267_apiTestDemo.py
+++ Ar_Script/ar_267_apiTestDemo.py
@@ -1,4 +1,5 @@
-from BeautifulReport import BeautifulReport
+#encoding=utf-8
+from BeautifulReport import BeautifulReport as bf
 import unittest
 import requests
 
@@ -10,8 +11,23 @@ class ApiTest(unittest.TestCase):
         pass
 
     def testBaidu(self):
+        '测试百度官网的状态码返回结果'
+        self.response=requests.get('http://www.baidu.com')
+        self.assertEqual(self.response.status_code,200)
+
+    def testBaidu2(self):
+        '测试百度官网的状态码返回结果'
+        self.response=requests.get('http://www.baidu.com')
+        self.assertEqual(self.response.status_code,200)
+
+    def testBaidu3(self):
+        '测试百度官网的状态码返回结果'
         self.response=requests.get('http://www.baidu.com')
         self.assertEqual(self.response.status_code,200)
 
 suite=unittest.TestSuite()
 suite.addTest(ApiTest('testBaidu'))
+suite.addTest(ApiTest('testBaidu2'))
+suite.addTest(ApiTest('testBaidu3'))
+runner=bf(suite)
+runner.report(filename='testreport',description='这是一个测试报告')
