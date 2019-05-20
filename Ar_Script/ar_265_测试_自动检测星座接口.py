88c36d0b1b84c068573a32f4b1ad384af16abd61
archerckk@163.com <a3203589>
2019-05-20 17:22:58 +0800

    完善星座接口检查测试用例逻辑跟日志打印

--- Ar_Script/ar_265_测试_自动检测星座接口.py
+++ Ar_Script/ar_265_测试_自动检测星座接口.py
@@ -11,26 +11,51 @@ class DailyTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    def testChinaApi(self):
-        self.responseCh=requests.get('http://api.dailyhoroscopepro.info/horoscope/article')
-        # self.response.request.get('http://api.apluscamera.info/v3/horoscopes/index/20190505/1')
-        print("星座汉化版接口请求返回码为：", self.responseCh)
-        regXml=re.compile('{"code":(\d+).+')
-        print('接口的返回内容为：', self.responseCh.text[0:20])
-        self.assertEqual(self.responseCh.status_code,200)
-        logging.debug(regXml.search(self.responseCh.text[0:30]).group(1))
-        self.assertNotEqual(regXml.search(self.responseCh.text[0:30]).group(1), 404)
 
+    def testChinaApi(self):
+        '检测国内版星座内容返回接口是否正常'
+        apiLink='http://api.dailyhoroscopepro.info/v3/horoscopes/index/20190520/4'
+        self.responseCh=requests.get(apiLink)
+        # self.response.request.get('http://api.apluscamera.info/v3/horoscopes/index/20190505/1')
+        print('接口连接：',apiLink)
+        print("星座汉化版接口请求返回码为：", self.responseCh)
+        regXml=re.compile('{"code":(%d).+'%404)
+        print('接口的返回内容为：', self.responseCh.text)
+        if regXml.search(self.responseCh.text)!=None:
+            contentCode = regXml.search(self.responseCh.text).group(1)
+        else:
+            contentCode=None
+
+        self.assertEqual(self.responseCh.status_code, 200)
+        if contentCode == None:
+            print('页面没有404状态码，测试通过')
+        else:
+            contentCode = regXml.search(self.responseEn.text).group(1)
+            print('code=', contentCode)
+            self.assertNotEqual(int(contentCode), 404)
+            print('页面有404错误')
 
     def testEnglishApi(self):
-        regXml=re.compile('{"code":(\d+).+')
+        '检测国际版星座数据内容返回接口是否正常'
+        apiLink = 'http://api.copohoroscopechina.info/horoscope'
+        self.responseEn = requests.get(apiLink)
+        print('接口连接：', apiLink)
+        print("星座国际版接口请求返回码为：", self.responseEn)
+        regXml = re.compile('{"code":(\d+).+')
+        print('接口的返回内容为：', self.responseEn.text)
+        if regXml.search(self.responseEn.text) != None:
+            contentCode = regXml.search(self.responseEn.text).group(1)
+        else:
+            contentCode = None
 
-        self.responseEn=requests.get('http://api.dailyhoroscopepro.info/v3/horoscopes/index/20190517/1')
-        print("星座中文版接口请求返回码为：", self.responseEn.status_code)
-        print('接口的返回内容为：', self.responseEn.text[0:20])
-        self.assertEqual(self.responseEn.status_code,200)
-        logging.debug(regXml.search(self.responseEn.text[0:30]).group(1))
-        self.assertNotEqual(regXml.search(self.responseEn.text[0:30]).group(1), 404)
+        self.assertEqual(self.responseEn.status_code, 200)
+        if contentCode == None:
+            print('页面没有404状态码，测试通过')
+        else:
+            contentCode = regXml.search(self.responseEn.text).group(1)
+            print('code=', contentCode)
+            self.assertNotEqual(int(contentCode), 404)
+            print('页面有404错误')
 
     def tearDown(self):
         pass
--- Ar_Script/ar_266_测试_运行自动检测星座.py
+++ Ar_Script/ar_266_测试_运行自动检测星座.py
@@ -9,10 +9,14 @@ from HTMLTestRunnerCN import HTMLTestReportCN
 from email.header import Header
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 import smtplib
 from BeautifulReport import BeautifulReport as bf
+from selenium import webdriver
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s-%(levelname)s-%(message)s')
+
+'测试集脚本配置'
 suite = unittest.TestSuite()
 suite.addTest(DailyTest('testChinaApi'))
 suite.addTest(DailyTest('testEnglishApi'))
@@ -23,16 +27,32 @@ logging.debug('报告路径为：', fileName)
 
 file = open(fileName, 'wb')
 
-runner=bf(suite)
+
 # runner = HTMLTestReportCN(
 #     stream=file,
 #     title='星座中文接口&星座英文接口数据返回测试报告',
 #     description='星座接口测试用例执行情况：'
 # )
+runner=bf(suite)
 runner.report(filename=os.sep + 'result' + os.sep + 'HoroscopesApiTest_' + now + '.html',description='星座接口测试用例执行情况：')
 file.close()
 time.sleep(2)
 
+'图片截图生成'
+screenShot='rusultScreenShot_%s.png'%now
+
+driver=webdriver.Chrome()
+driver.get(fileName)
+driver.maximize_window()
+driver.implicitly_wait(5)
+'控制页面滑动到底部'
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+'截图'
+driver.get_screenshot_as_file(screenShot)
+time.sleep(2)
+driver.quit()
+
+'邮件参数设置'
 smtpServer='smtp.avazu.net'
 smtpAccount='zhibin.chen@dotcunited.com'
 smtpPsw='Chenzhibin2019'
@@ -41,14 +61,16 @@ with open('mailList.txt')as f:
     for i in f:
         target_list.append(i)
 
-message=MIMEMultipart()
-message['from']=Header('zhibin.chen@dotcunited.com','utf-8')
+'邮件的基础参数'
+message=MIMEMultipart('related')
+message['from']=smtpAccount
 message['to']=Header(target_list[1],'utf-8')
-message['subject']=Header('Re:星座中文接口和星座英文接口数据返回测试报告','utf-8')
+message['subject']=Header('Re:%s_星座中文接口和星座英文数据返回接口测试报告'%now,'utf-8')
 
-sendFileContent=open(fileName,'rb').read()
+
 
 '发送附件构造'
+sendFileContent=open(fileName,'rb').read()
 att=MIMEText(sendFileContent,'base64','utf-8')
 #这是一个未知的类型
 att['Content-Type']='application/octet-scream'
@@ -56,14 +78,29 @@ att['Content-Disposition']='attachment; filename=%s'%fileName
 message.attach(att)
 
 '发送文本内容构造'
-att2=MIMEText(open(fileName,'rb').read(),'html','utf-8')
-message.attach(att2)
+msg_alterNative=MIMEMultipart('alternative')
+message.attach(msg_alterNative)
+mailMsg="""
+<p><img src="cid:result"></p>
+"""
+msg_alterNative.attach(MIMEText(mailMsg,'html','utf-8'))
+
+'图片对象构造'
+image=open(screenShot,'rb')
+mimeImage=MIMEImage(image.read())
+image.close()
+'添加图像ID，让'
+mimeImage.add_header('Content-ID','<result>')
+message.attach(mimeImage)
+
 
 '邮件发送'
+logging.debug('开始发送邮件')
 try:
     smtp=smtplib.SMTP(smtpServer)
     smtp.login(smtpAccount,smtpPsw)
     smtp.sendmail(smtpAccount,target_list[1],message.as_string())
     smtp.quit()
+    logging.debug('发送成功')
 except smtplib.SMTPException:
     print('发送失败')
