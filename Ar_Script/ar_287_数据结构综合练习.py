05494a902fb9f43d6eb7e37a971bbb3fda78a088
archerckk <archerckk@163.com>
2019-06-17 08:51:11 +0800

    数据结构综合练习代码提交

--- Ar_Script/ar_287_数据结构综合练习.py
+++ Ar_Script/ar_287_数据结构综合练习.py
@@ -51,3 +51,43 @@ b=list(reversed(a))
 c=''
 c=c.join(b)
 print(c)
+
+#1.6 去除a字符串内的数字后，请将该字符串里的单词重新排序（a-z），并且重新输出一个排序后的字符 串。（保留大小写,a与A的顺序关系为：A在a前面。例：AaBb）
+a = list("aAsmr3idd4bgs7Dlsf9eAF")
+stripNum=a[:]
+for i in stripNum:#去除a字符串内的数字
+    if i.isdigit():
+        stripNum.remove(i)
+
+stripNum.sort()
+print(''.join(stripNum))
+
+
+#1.7 请判断 'boy'里出现的每一个字母，是否都出现在a字符串里。如果出现，则输出True，否则，则输 出False.
+resultList=[]
+for i in 'girl':
+    if i in stripNum:
+        resultList.append('True')
+    else:
+        resultList.append('False')
+if 'False' in resultList:
+    print('False')
+else:
+    print('True')
+
+#1.8 要求如1.7，此时的单词判断，由'boy'改为四个，分别是 'boy','girl','bird','dirty'，请判断如上这4个字符串里的每个字母，是否都出现在a字符串里。
+resultList=[]
+checkList=['boy','girl','bird','dirty']
+print('1.8结果展示')
+
+for i in checkList:
+    for j in i:
+        if j in stripNum:
+            resultList.append('True')
+        else:
+            resultList.append('False')
+    if 'False' in resultList:
+        print('False')
+    else:
+        print('True')
+    resultList=[]
