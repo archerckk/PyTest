d5779caa51663e22b88d09f45acd58fa3b228d0f
archerckk <archerckk@163.com>
2019-06-22 09:39:00 +0800

    装饰器计时器代码提交

---
+++ Ar_Script/ar_289_进阶_装饰器计时器.py
@@ -0,0 +1,25 @@
+#encoding=utf-8
+
+import time
+import functools
+
+def timeCounter(fun):
+    @functools.wraps(fun)
+    def warp(*args,**kwargs):
+        start=time.perf_counter()
+        run=fun()
+        end=time.perf_counter()
+        result=end-start
+        print('{}执行了{}秒'.format(fun.__name__,result))
+
+        return run
+    return warp
+
+@timeCounter
+def realFun():
+    result=0
+    for i in range(10000000):
+        result=result+i
+    return result
+
+print(realFun())
