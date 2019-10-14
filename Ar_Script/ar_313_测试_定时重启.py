4a4eb7c7b9bedbf08381ec3c725ab1a99bb0fc83
archerckk@163.com <a3203589>
2019-10-14 14:54:26 +0800

    修改重启输入方式

--- Ar_Script/ar_313_测试_定时重启.py
+++ Ar_Script/ar_313_测试_定时重启.py
@@ -1,9 +1,11 @@
 import os
 import time
 
+
+result=int(input('请输入重启的时间：'))
 while True:
     startTime=time.localtime()
-    if startTime[3]==9:
+    if startTime[3]==result:
         os.popen('shutdown -r -t 10')
     else:
         continue
