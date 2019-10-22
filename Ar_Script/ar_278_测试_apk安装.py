b5415884e5a8805fc67a9f074862b1ec94866fc1
archerckk@163.com <a3203589>
2019-10-22 16:58:46 +0800

    修复安装脚本的targetSDK版本显示不全问题

--- Ar_Script/ar_278_测试_apk安装.py
+++ Ar_Script/ar_278_测试_apk安装.py
@@ -12,35 +12,26 @@ def getPackagInfo():
     add_time="_{}".format(time.localtime()[5])
 
     filePath = g.fileopenbox(msg=msg, title=title, default=default)
-    file_dir = os.path.dirname(filePath)
-    # print(file_dir)
-
-    if ' ' in filePath:
-        fileNewName = filePath.replace(' ', '_')
-    if "&" in filePath:
-        fileNewName = filePath.replace('&', '_')
-    if "&&" in filePath:
-        fileNewName = filePath.replace('&&', '_')
 
     fileNewName=filePath.split('.apk')[0].strip()+add_time+'.apk'
     os.rename(filePath,fileNewName)
 
     print('选择的apk路径为：', fileNewName)
-    # command = 'aapt dumpsys badging %s > packageInfo.txt' % fileNewName
+
     command = 'aapt dumpsys badging "%s" ' % fileNewName
-    # packInfoFile = './packageInfo.txt'
 
     handle = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     time.sleep(2)
 
     reg_packageName = re.compile(r"package: name='(.+?)'")
     reg_launchableActivity = re.compile(r"launchable-activity: name='(.+?)'")
-    # log = ''
+
     log=str(handle.stdout.read())
-    print('\n',log[0:176],'\n')#截取到targetSDK Version部分包信息
-    # with open(packInfoFile, encoding='utf-8',errors='ignore')as f:
-    #     for i in f:
-    #         log += i
+    position=log.index('targetSdkVersion:')
+
+
+    print('\n',log[0:position+21],'\n')#截取到targetSDK Version部分包信息
+
     try:
         packageName = reg_packageName.search(log).group(1)
         lanuchableActivity = reg_launchableActivity.search(log).group(1).strip()
@@ -48,7 +39,6 @@ def getPackagInfo():
         return fileNewName, packageName, lanuchableActivity
     except Exception as err:
         print(err)
-    # print(lanuchableActivity)
 
 
 
@@ -86,5 +76,5 @@ if __name__ == '__main__':
     handle_install = installapp(filePath)
 
     print('安装日志为：', handle_install.stdout.read().decode().strip('\r\n'))
-    # os.remove('./packageInfo.txt')
+
     input('安装完成,按回车关闭窗口')
