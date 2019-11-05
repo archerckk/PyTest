8375cc40ba40f43b140b57b8f623cc359a02ecf5
archerckk@163.com <a3203589>
2019-11-05 20:02:20 +0800

    微博代码提交，作业整理

--- Ar_Script/ar_323_练习_wisg.py
+++ Ar_Script/ar_323_练习_wisg.py
@@ -2,7 +2,7 @@ from wsgiref.simple_server import make_server
 from ar_324_练习_wisg_show import application
 
 #配置服务器
-httpd=make_server('',8000,application)
+httpd=make_server('',8001,application)
 
 print('Server HTTP on port 8000...')
 
