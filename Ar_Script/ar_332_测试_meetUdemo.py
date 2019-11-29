7a02cd6dbcf2bb312477c5d0ee3dbe5b6af0818a
archerckk@163.com <a3203589>
2019-11-29 20:09:55 +0800

    新增like接口用例

--- Ar_Script/ar_332_测试_meetUdemo.py
+++ Ar_Script/ar_332_测试_meetUdemo.py
@@ -3,38 +3,40 @@ import requests
 import logging
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s-%(levelname)s-%(message)s')
 
+
+
+environment='test'
+if environment == 'test':
+    host = 'http://150.109.38.68'
+    token='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjVkODM0MTc1N2VjZjIyZjQ5OTAzNDc0NjFjOTFmZGIxZmQwYmJiNTg2NWJiNWU0MDZmZTEyMzdlMGIwYTBkMjc1ZDA5YWMyZGE4YjAwMTc0In0.eyJhdWQiOiIyIiwianRpIjoiNWQ4MzQxNzU3ZWNmMjJmNDk5MDM0NzQ2MWM5MWZkYjFmZDBiYmI1ODY1YmI1ZTQwNmZlMTIzN2UwYjBhMGQyNzVkMDlhYzJkYThiMDAxNzQiLCJpYXQiOjE1NzUwMjM3ODUsIm5iZiI6MTU3NTAyMzc4NSwiZXhwIjoxNTc2MzE5Nzg1LCJzdWIiOiI0MjMiLCJzY29wZXMiOltdfQ.C4k3UjIoYa6moWxJjjp9mraBzsugEFBl92j5FiEmwn_fJQHkfPoWyteGy4uSr4N0erFKGIErNzhY0R4WK4F1WpvKzekNSYsIGIeX47cVzb4UXSJzd7MQ7uqVJL3_miRAu0e-Ofm9vcCq_hqtuX6wbpQsSdvvKosBAETtfnutjkAQc-rJALvG2KR2tr90fnXbgCSE_voxieOWrQUgnBIR_KvUL9cZ6E-RrtkoHYei7Kp41B-QBMmg48aXsKz9wjvLG3DJNXQnIlwJjBdAa3hTk0s-HPzTB4cih8hGcTUcFfRY5AI7J98SI-777jJ6q4ECBO88LfOcgVy6IWOCXcMQDXlT7KZ9silx2z2CkWvuSuR-3qTOV1Gi2FW3pIpbgQkIJUx48i-Lk01i14NXegxfT4lr9yp7MOiz6IlEL9GmytWP6X_dvroh_EzZunOcH_hp0C6Mo_txwM5m5NJ7qbEAGrPbuPKPrIlYmwA6AMpudp42mSryYMEClccwaXrt0EZiVC3QZkJaIQYyFkXb9YPRCyRaFZMML1ykUxKyXzWCe5Iw0-eewx6kDLxKpyn3l2NEC6QIGW5b_LjyIIlcBVGgJHOEkJTkE1kX6lhdrt76GPS-_RcMckvga6DTLvsla2DCgYkMYGxQ9mF57gKQqoM5-9jRDxVMpn9essDCdE7hI7s'
+    # token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQzZTRhZDU5NzQ4NzU1ZjMwODI5YmYyNDQzN2JhODViODY3ODNhNzhhYmZhY2I4MGI3ODY1YTQ5OWJiNmVjZmU1MTM2ZjlmNzhiZDg5N2NhIn0.eyJhdWQiOiIyIiwianRpIjoiZDNlNGFkNTk3NDg3NTVmMzA4MjliZjI0NDM3YmE4NWI4Njc4M2E3OGFiZmFjYjgwYjc4NjVhNDk5YmI2ZWNmZTUxMzZmOWY3OGJkODk3Y2EiLCJpYXQiOjE1NzQ0Mjk3NDgsIm5iZiI6MTU3NDQyOTc0OCwiZXhwIjoxNTc1NzI1NzQ4LCJzdWIiOiI0MjkiLCJzY29wZXMiOltdfQ.COE8VPyJQOJSPWEG0i_iOcm81Rg0Y9a5gCOyeuHJfqHdvsHAOm2jkAA6nfU_WhmqXrZlNA64GrqNi9BjE8sFlMUcwRBl5EjJap_p2X33uxBtM-u4Ge1nr3H8phBYPZK6sOmGSMYuwWFo6fdem3wot5swz2eKGKrCA1Pn4aSH5L87vidfF2-FJfo5Zl45ePrzr68__MkqQyce7LWj-SGxUCejkBv7PTJputtg-7Azz8GJKl4QAyOAmhCqFTwXE4dy29jyMqdBKDEf1cTlhhiNayyLLY_m4om-MtVj9KNZWY09xnMq9WBynKU3_s_nSCLVH2po0jfUt1_o62TWKL1UAN9fBGY9Zl6d-jIwVt8WUVoJikaUahXLFzGaF880nGmYSxs_DG2-VRrLAhlm5lWfvwYH3DfGBOFp-Imk9a7sBpIKZAJ2kIjPl6Oo-RapPTNwAE3rsYsSQD0igt5_jtn55BfMKspn8RJf6leKsTCs0lSOe2qpnvU94SUTnbGbqNaLZrvDDYJQu_BRO5uZWbBDElETimMf4q4dQMEvHSC-NqiViiisgsHyztDKUsrcz_orfEmxw2yGeumobpKDt8jc0HNEJ3mPQnZKV7zs6KrDNlwDbKNvreV-ARt6rZRuLhTuKjQcKcmR0nFB6KwlfDvOfYC7AbJYdfAqFjEbhw5DE0A'
+elif environment == 'normal':
+    token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImY3NGEzNTUwMTdkYjhkODBiOWMyMzFkNDhhMTc4ZDI1YjE5NTc2YzlkMGQ2ZDJkNWU0Nzk3MmQzMzgzYmNiYzQzYzdkNTgzMmVkYTBiZGFkIn0.eyJhdWQiOiIxIiwianRpIjoiZjc0YTM1NTAxN2RiOGQ4MGI5YzIzMWQ0OGExNzhkMjViMTk1NzZjOWQwZDZkMmQ1ZTQ3OTcyZDMzODNiY2JjNDNjN2Q1ODMyZWRhMGJkYWQiLCJpYXQiOjE1NzQ4NTc2MDgsIm5iZiI6MTU3NDg1NzYwOCwiZXhwIjoxNjA2NDgwMDA4LCJzdWIiOiI2MiIsInNjb3BlcyI6W119.fj92n_Rl1w_dG985eH6sv4YbqG1cccu3Rv0-GDhgsJ3bwmBT3gLamNHYNyw9RYXvezrIhIhgSvXrVffiVUTFnC6tWcG6_tMF2Hl5Xa8n-6qz4Bm0j-gJx5SJie8UDjzwTaSu6UP2tC8JL3uuq5fL_Pf35OPaOaHBjBoM6qP-iskUd3qQwyWw-c1eNQfK-Yrz-0iZSOGGYRHvR3DWh4GIAbVr7CAkBNElJ0vjAMxAJowda5et3_eQ0yHLwLRi3AiAImX7Qk_3dzPOoqE9UFqHMUrGiJUoVVvIOiVAeWq1LxrXninCRuTNEmfWmu915CF7suc79rS_avj-ZED1VlOYzUwhQhgxzHwnhpsBTvK5rkTciqwVkxiP9pxP1SnOKHxzJPd3-8yNCKM03QXpsKUGqzSQgHVkYFJODYoWgy7wZlLIoOWk5zWbN2r5EKWXdJQrcxW3skwLuoOxyipVNlaQgLAdKGrNH7ZT2KO82SnRUUluJoMWV-yzjOhsGmldMjAdpT_6OoMxhydQfWzg4WpBqvYeFCSO6M2QNZuSCTEtdw8OFNGTwmekW8xiu7bv1lAQ5JsvQr11_LziDrBSne-fyCnwDHaod0YeS8hp2xNdiPO7298Rz2LLQOP28KtK8X5TeA6Fv155YE9KblilCyz49Cc90eyE1K1ZAscERRyEwAU'
+    host = 'http://www.meetuapp.info'
+
 class TestDemo(unittest.TestCase):
-    def setUp(self) -> None:
-        environment = 'test'
-        if environment == 'test':
-            self.host = 'http://150.109.38.68'
-        elif environment == 'normal':
-            self.host = 'http://www.meetuapp.info'
+
 
     def test_update_profile(self):
         headers = {
-             "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjE1NTJlNzBmMTY5ZGQ5OWI0MWYzNmQ3OTE4ZDQ5YzZiYzNiYjE2MzZhZjE3OGM1NzBkMDFhOTZkYmNkNGZjODViZmQ5ZTVmMGEyNjU3YTY3In0.eyJhdWQiOiIyIiwianRpIjoiMTU1MmU3MGYxNjlkZDk5YjQxZjM2ZDc5MThkNDljNmJjM2JiMTYzNmFmMTc4YzU3MGQwMWE5NmRiY2Q0ZmM4NWJmZDllNWYwYTI2NTdhNjciLCJpYXQiOjE1NzQ3MjQyMTksIm5iZiI6MTU3NDcyNDIxOSwiZXhwIjoxNTc2MDIwMjE5LCJzdWIiOiI0MjMiLCJzY29wZXMiOltdfQ.oj0zj3H6wLqiG2_juT0KiOLXATy5yWj9IWsD9j1q-QH-EVq196Rx9rWw8HRVSO7__X3-2k2U2e0ykgpOdRClPe03D3dngqVnmQmCRPUPs23q19yuWzOnmpurFVLWwpt5_PiYHrVa91JSq5HboA6QSwdafX6iM2cuRwsjXZ4xzzcj29tqIgtIXblGwzc7QejWN4KdnW13sHUOMnzJVK5XU7AMNmk7NGPolgbIphoW5Zi2JwLyQdt4h9w61hs9eEUFfqXifOGdfpmT67vK-0Me56UPQQ6nIm0QlJVDHooTYYS7z9iykzmHYTutfo4_ETrI76TDi3m2tCF2gEtBUEnyf4GwrYd-4KYTeh0BDSEQwwPo6fi-TnwBVzAWpQGTArXrOll0MMvIYXoEgMlRKN-JT6fRm0ghPmfFry8tz02jaAdm45348jxf6u5vsXFMkW0OM8fgOdpZ5VLUrsV_RpJlGZs8qaKlJsXOG_yHcuZ2paO5cshz5POIgH0wf6IAqguMsjg38L8u0OqL824R1BPS1yR-Y8uEf_HhngcOqK5jeNs9ua1qLYpZQSp1Llvjdk1E3VpApLCInJLcGqxLemPJZEUcdQ7CBvxaEVRP7ijJgDMhTQZi_FQdhjWxwRgoZn2fGGxWQ3DFJ9a3vSu5-hKgq8Lbhc1nBhFBFUZl9VEiAhA",
-             "Host": "150.109.38.68",
-             "Connection": "Keep-Alive",
-             "Accept-Encoding": "gzip",
-             "User-Agent": "okhttp/3.12.0",
-             "X-Requested-With": "XMLHttpRequest"
+        "Authorization":token,
+        "Accept": "application/json"
         }
 
-        # data={
-        #     "block_id": "385",
-        #     "status": "block"
-        # }
+        data={
+        "chat_user_id": "429",
+        }
 
-        url = '{}/api/user/match_list/429'.format(self.host)
+        url = '{}/api/user/instant_chat'.format(host)
 
-        response=requests.get(url,headers=headers)
+        response=requests.post(url,headers=headers,data=data)
 
         result_json = response.json()
 
         logging.debug('返回内容为：{}'.format(result_json))
 
-        self.assertEqual(result_json['message'],'success')
-        self.assertEqual(result_json['status'],True)
-        self.assertTrue(result_json['data'] is not None)
+
+        # self.assertEqual(result_json['status'],'DISLIKED')
+        # self.assertEqual(result_json['statuscode'],3)
+        # self.assertEqual(result_json['message'] ,'User disliked successfully')
         # logging.debug('提示信息：{}'.format(result_json['message']))
