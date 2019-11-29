7b15717f47874e474b04362ebf33f6172afa74f7
archerckk <archerckk@163.com>
2019-11-29 09:47:31 +0800

    格式优化

--- Ar_Script/ar_225_requests_例子.py
+++ Ar_Script/ar_225_requests_例子.py
@@ -1,6 +1,7 @@
 import requests
-r=requests.get('https://api.github.com/user',auth=('archerckk@163.com',
-                                                        'a3203589'))
+r=requests.get('https://api.github.com/user',
+               auth=('archerckk@163.com','a3203589')
+               )
 print('状态码为：%s'%r.status_code)
 print(r.headers['content-type'])
 print(r.encoding)
