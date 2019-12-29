b852a931c4781021f2e17bb2356cf377325ce38f
archerckk <archerckk@163.com>
2019-12-29 20:57:14 +0800

    完善用户信息接口

--- Ar_Script/ar_341_meetu_api.py
+++ Ar_Script/ar_341_meetu_api.py
@@ -7,11 +7,11 @@ import pytest
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s-%(levelname)s-%(message)s')
 
-environment = 'normal'
+environment = 'test'
 
 if environment == 'test':
     host = 'http://150.109.38.68'
-    token='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjVkODM0MTc1N2VjZjIyZjQ5OTAzNDc0NjFjOTFmZGIxZmQwYmJiNTg2NWJiNWU0MDZmZTEyMzdlMGIwYTBkMjc1ZDA5YWMyZGE4YjAwMTc0In0.eyJhdWQiOiIyIiwianRpIjoiNWQ4MzQxNzU3ZWNmMjJmNDk5MDM0NzQ2MWM5MWZkYjFmZDBiYmI1ODY1YmI1ZTQwNmZlMTIzN2UwYjBhMGQyNzVkMDlhYzJkYThiMDAxNzQiLCJpYXQiOjE1NzUwMjM3ODUsIm5iZiI6MTU3NTAyMzc4NSwiZXhwIjoxNTc2MzE5Nzg1LCJzdWIiOiI0MjMiLCJzY29wZXMiOltdfQ.C4k3UjIoYa6moWxJjjp9mraBzsugEFBl92j5FiEmwn_fJQHkfPoWyteGy4uSr4N0erFKGIErNzhY0R4WK4F1WpvKzekNSYsIGIeX47cVzb4UXSJzd7MQ7uqVJL3_miRAu0e-Ofm9vcCq_hqtuX6wbpQsSdvvKosBAETtfnutjkAQc-rJALvG2KR2tr90fnXbgCSE_voxieOWrQUgnBIR_KvUL9cZ6E-RrtkoHYei7Kp41B-QBMmg48aXsKz9wjvLG3DJNXQnIlwJjBdAa3hTk0s-HPzTB4cih8hGcTUcFfRY5AI7J98SI-777jJ6q4ECBO88LfOcgVy6IWOCXcMQDXlT7KZ9silx2z2CkWvuSuR-3qTOV1Gi2FW3pIpbgQkIJUx48i-Lk01i14NXegxfT4lr9yp7MOiz6IlEL9GmytWP6X_dvroh_EzZunOcH_hp0C6Mo_txwM5m5NJ7qbEAGrPbuPKPrIlYmwA6AMpudp42mSryYMEClccwaXrt0EZiVC3QZkJaIQYyFkXb9YPRCyRaFZMML1ykUxKyXzWCe5Iw0-eewx6kDLxKpyn3l2NEC6QIGW5b_LjyIIlcBVGgJHOEkJTkE1kX6lhdrt76GPS-_RcMckvga6DTLvsla2DCgYkMYGxQ9mF57gKQqoM5-9jRDxVMpn9essDCdE7hI7s'
+    token='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijc3MzlkNDBlNzM0NDZjMTI1MTdkMzUzNzZjYTdlM2IzMjhjN2RhZGMzMjA0YmM0MmU0MDJiMTk4NzVjMDZhNzBmYWE3NWVjYTI5ZDExMzlkIn0.eyJhdWQiOiIyIiwianRpIjoiNzczOWQ0MGU3MzQ0NmMxMjUxN2QzNTM3NmNhN2UzYjMyOGM3ZGFkYzMyMDRiYzQyZTQwMmIxOTg3NWMwNmE3MGZhYTc1ZWNhMjlkMTEzOWQiLCJpYXQiOjE1NzcxMDA1NDYsIm5iZiI6MTU3NzEwMDU0NiwiZXhwIjoxNTc4Mzk2NTQ2LCJzdWIiOiI0MjkiLCJzY29wZXMiOltdfQ.YA4qIweJU8AAwzyf76o1cai9S7uY3Owk7gQDFD6HSluvZAj5Tzw4Ma9ZAsDoLXuanNrVQZgzs3S4XaCkE2sQOEQFCXiAwUB28pPQgURy_o7obPhDFM68Aozgrzq9eEKoGYxK9gMLKW6AFVY128DmmpRC4iWshmaFguuFv3RgjsVU9fcJ8Wfr4TnVXqafZk_xvFrU9Yk24_2RLpVno8SMgUpovDucqoscxNc8EI5YiSE4gOC0_BorFXhhm8oSE46RFP_PCqOzz4gJCwbM0CJjVgnViTMsqAGnzSHfE1JI9J5Y-Rd05HAzwJ4lQqAskYzRlZYy2wEcY7ob7fj9qhNMDrWgc68TaEgRP7Mspx82gx-m4t_OxPRR5M2g1OuKz_Vik8x3Bg7z6jaL4A3lqneWp7oMHQAx6FGiJ15Q-dg-WwRwr7Hbg6u9SUqLaO5v1dRCnOcc7r0-LnlDI_Sl9L21jWV4xfrJmVuyS7FT9nh4HdBLVd0UMl7Y3wo4sw__9P9LMrf8UsiTgCGhLGzaH1ed2UfhLManpGY1HwmWs1BVvvAbfqRU7EfzKIRJP-QQZG-Ralm86QCFbiN2CEAmypSRA8iPYeuG0GPAzvKay2f_6ruM1c_xx18medOCMSqzo6-i9F1ximpwwogMrafRohuJ4Y3KXI5kSCo772Pffgf4L7o'
 elif environment == 'normal':
     host = 'http://www.meetuapp.info'
     token='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImY3NGEzNTUwMTdkYjhkODBiOWMyMzFkNDhhMTc4ZDI1YjE5NTc2YzlkMGQ2ZDJkNWU0Nzk3MmQzMzgzYmNiYzQzYzdkNTgzMmVkYTBiZGFkIn0.eyJhdWQiOiIxIiwianRpIjoiZjc0YTM1NTAxN2RiOGQ4MGI5YzIzMWQ0OGExNzhkMjViMTk1NzZjOWQwZDZkMmQ1ZTQ3OTcyZDMzODNiY2JjNDNjN2Q1ODMyZWRhMGJkYWQiLCJpYXQiOjE1NzQ4NTc2MDgsIm5iZiI6MTU3NDg1NzYwOCwiZXhwIjoxNjA2NDgwMDA4LCJzdWIiOiI2MiIsInNjb3BlcyI6W119.fj92n_Rl1w_dG985eH6sv4YbqG1cccu3Rv0-GDhgsJ3bwmBT3gLamNHYNyw9RYXvezrIhIhgSvXrVffiVUTFnC6tWcG6_tMF2Hl5Xa8n-6qz4Bm0j-gJx5SJie8UDjzwTaSu6UP2tC8JL3uuq5fL_Pf35OPaOaHBjBoM6qP-iskUd3qQwyWw-c1eNQfK-Yrz-0iZSOGGYRHvR3DWh4GIAbVr7CAkBNElJ0vjAMxAJowda5et3_eQ0yHLwLRi3AiAImX7Qk_3dzPOoqE9UFqHMUrGiJUoVVvIOiVAeWq1LxrXninCRuTNEmfWmu915CF7suc79rS_avj-ZED1VlOYzUwhQhgxzHwnhpsBTvK5rkTciqwVkxiP9pxP1SnOKHxzJPd3-8yNCKM03QXpsKUGqzSQgHVkYFJODYoWgy7wZlLIoOWk5zWbN2r5EKWXdJQrcxW3skwLuoOxyipVNlaQgLAdKGrNH7ZT2KO82SnRUUluJoMWV-yzjOhsGmldMjAdpT_6OoMxhydQfWzg4WpBqvYeFCSO6M2QNZuSCTEtdw8OFNGTwmekW8xiu7bv1lAQ5JsvQr11_LziDrBSne-fyCnwDHaod0YeS8hp2xNdiPO7298Rz2LLQOP28KtK8X5TeA6Fv155YE9KblilCyz49Cc90eyE1K1ZAscERRyEwAU'
@@ -63,8 +63,9 @@ class Test_meetU_API:
             assert content['code']==200
             assert content['status']==True
 
-
-    def test_user_app(self):
+    @allure.story('用户信息获取接口测试')
+    @pytest.mark.parametrize('value', [token, '', '123'])
+    def test_user_app(self,value):
         logging.debug('用户信息获取接口_user /user_app测试开始')
         headers={
         "Authorization": token,
@@ -90,10 +91,12 @@ class Test_meetU_API:
         responese=requests.post(url,headers=headers,data=data)
 
         result=responese.text
+
+
         logging.debug('返回内容为：{}'.format(result))
 
-        self.assertTrue('"message":"success"'in result)
-        self.assertTrue('"user_id":423' in result)
+        assert '"message":"success"'in result
+        assert '"user_id":429' in result
 
     @data(
         token,
