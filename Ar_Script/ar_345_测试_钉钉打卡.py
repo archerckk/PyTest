befa8e48bf03b695e1d1b90d4dc601723b0c4768
archerckk <archerckk@163.com>
2020-01-11 09:04:02 +0800

    新增三星c8 JSON，钉钉打卡逻辑新增

--- Ar_Script/ar_345_测试_钉钉打卡.py
+++ Ar_Script/ar_345_测试_钉钉打卡.py
@@ -1,10 +1,27 @@
 from appium import webdriver
 import json
+import time
 
 class AutoClick(object):
 
     def __init__(self):
         with open('./resources/phone.json')as f:
-            desired_caps=json.load(f)
+            desired_caps=json.load(f)['sanxingC8_dingding']
 
-        self.driver=webdriver.Remote()
+        self.driver=webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+    def work(self):
+        #点击工作tab
+        self.driver.find_element_by_xpath('//*[@resource-id="com.alibaba.android.rimet:id/home_bottom_tab_icon"][@instance=8]').click()
+        time.sleep(5)
+        #点击考勤打卡按钮
+        self.driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/com.uc.webview.export.WebView/com.uc.webkit.be/android.webkit.WebView/android.view.View/android.view.View[3]/android.view.View[3]/android.view.View[8]/android.view.View[1]/android.view.View[1]/android.view.View').click()
+        # time.sleep(5)
+        # self.driver.find_element_by_xpath('//*[@class="android.view.View"][@instance=127]').click()
+
+
+
+if __name__ == '__main__':
+    ak=AutoClick()
+    ak.work()
+
