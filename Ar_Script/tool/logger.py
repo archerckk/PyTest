73b1bbf418b2e6bd9b2411d54648ce8112aa6996
archerckk <archerckk@163.com>
2020-03-12 11:00:23 +0800

    优化logger模块的代码逻辑

--- Ar_Script/tool/logger.py
+++ Ar_Script/tool/logger.py
@@ -4,7 +4,7 @@ from logging import handlers
 
 class Loger(object):
 
-    def __init__(self, fileName, level="debug", mode='w',error_record=False):
+    def __init__(self, fileName='test', level="debug", mode='w',error_record=False,file_save=False):
         self.log = logging.getLogger()
 
         self.level_dict = {
@@ -16,7 +16,6 @@ class Loger(object):
         }
 
         # 2、设置文件跟屏幕输出的handler
-        self.filehandler = logging.FileHandler(filename=fileName + '.log', mode=mode, encoding="utf-8")  # 默认
         # self.filehandler=handlers.TimedRotatingFileHandler(filename=fileName,when='D',backupCount="3",encoding='utf-8')
         self.streamhandler = logging.StreamHandler()
 
@@ -25,17 +24,20 @@ class Loger(object):
         formatter = logging.Formatter(fmt="%(asctime)s-%(levelname)s-%(message)s")
 
         # 4.绑定关系：①logger绑定handler
-        self.log.addHandler(self.filehandler)
         self.log.addHandler(self.streamhandler)
 
         # ②为handler绑定formatter
-        self.filehandler.setFormatter(formatter)
         self.streamhandler.setFormatter(formatter)
 
         # 5、设置日志级别各个对象的日志级别
         self.log.setLevel(self.level_dict[level])
-        self.filehandler.setLevel(self.level_dict[level])
         self.streamhandler.setLevel(self.level_dict[level])
+
+        if file_save:
+            self.filehandler = logging.FileHandler(filename=fileName + '.log', mode=mode, encoding="utf-8")            # 默认
+            self.log.addHandler(self.filehandler)
+            self.filehandler.setFormatter(formatter)
+            self.filehandler.setLevel(self.level_dict[level])
 
         if error_record:
             self.errorhandler = logging.FileHandler(filename=fileName + ".error_log", mode=mode, encoding="utf-8")
