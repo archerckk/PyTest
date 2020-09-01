13ecdb0d6d7b3f674654225770ca9aea73f41335
archerckk <archerckk@163.com>
2020-09-01 17:10:03 +0800

    日志清线新增模式判断

--- Ar_Script/ar_375_test_日志清洗2.py
+++ Ar_Script/ar_375_test_日志清洗2.py
@@ -3,16 +3,20 @@ import pprint
 import json
 
 content=''
-with open('resources/text.txt')as f:
+with open('text.txt')as f:
     for i in f:
         content+=i
+while 1:
+    mode=input('请选择你的模式:')
+    reg_break=re.compile(r'2020-.+com.social.nene I/LovU: ')
+    tmp_content=re.sub(reg_break,'',content)
+    new_content=tmp_content.replace(' ','').replace('\n','')
+    if mode=='1':
+        json_result = json.loads(new_content)
+        pprint.pprint(json_result)
+    else:
+        print(new_content)
 
-reg_break=re.compile(r'2020-.+com.social.nene I/LovU: ')
-tmp_content=re.sub(reg_break,'',content)
-new_content=tmp_content.replace(' ','').replace('\n','')
-
-json_result=json.loads(new_content)
-pprint.pprint(json_result)
 
 # with open('resources/text.txt','w')as f:
 #     f.write(new_content)
