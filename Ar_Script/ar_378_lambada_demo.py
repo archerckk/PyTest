2db9f8fffa6df069a001b7bbaa41ec79531a9262
archerckk@163.com <archerckk@163.com>
2021-02-09 08:11:36 +0800

    提交字典排序练习代码

---
+++ Ar_Script/ar_378_lambada_demo.py
@@ -0,0 +1,13 @@
+import  json
+
+# 字典排序
+dict1={'a':1,'d':3,'c':2}
+dict1_to_list=list(dict1.items())
+dict1_to_list.sort(key=lambda k:k[1])
+print(dict(dict1_to_list))
+
+
+# 字符串转换成功json格式
+str1='{"a":1,"d":3,"c":2}'
+json_result=json.loads(str1)
+print(type(json_result),json_result)
