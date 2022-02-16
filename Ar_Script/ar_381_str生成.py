6de8b602dec232acdbf94337006dc18bc39e92be
chenzhibin <>
2022-02-16 22:38:01 +0800

    新增生成指定长度字符串脚本；

---
+++ Ar_Script/ar_381_str生成.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2022/2/16 14:31
+# @Author : Zhibin
+# @Description : 生成指定长度字符串
+
+a='a'
+times=500
+for i in range(500):
+    a+="a"
+
+
+print(a)
